144508618806e9ad0e0d955c6902000637e0c42e
pmedved <philipp.medved@gmx.at>
2018-06-12 19:24:05 +0200

    adding Sockets

--- src/main/java/Client/ClientController.java
+++ src/main/java/Client/ClientController.java
@@ -6,6 +6,11 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ClientController {
 
@@ -23,6 +28,23 @@ public class ClientController {
 
     private String username = "";
 
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientController() {
+        try {
+            clientSocket = new Socket("127.0.0.1",2231);
+            //Rausschicken
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            //Empfangen
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @FXML
     private void send(){
@@ -31,11 +53,22 @@ public class ClientController {
             onlineUsers.setText(onlineUsers.getText() + "\n" + username);
         }
 
-        if(mainTextArea.getText().equals("")) {
-            mainTextArea.setText(username + ": " +inputTextArea.getText());
-        }else{
-            mainTextArea.setText(mainTextArea.getText() + "\n" + username + ": " +inputTextArea.getText());
+        out.println(username + ": " +inputTextArea.getText());
+
+        String resp = "Error getting Response";
+
+        try {
+            resp = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        if(mainTextArea.getText().equals("")) {
+            mainTextArea.setText(username + ": " + resp);
+        }else{
+            mainTextArea.setText(mainTextArea.getText() + "\n" + username + ": " + resp);
+        }
+
         inputTextArea.clear();
 
     }
--- src/main/java/Server/ServerController.java
+++ src/main/java/Server/ServerController.java
@@ -1,4 +1,37 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerController {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public  ServerController(){
+        System.out.println("seas");
+        //Man braucht den Thread damit .accept nicht alles Blockiert
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        serverSocket = new ServerSocket(2231);
+                        clientSocket = serverSocket.accept();
+                        out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+
+    }
 }
