4211ebd99b2c90b6671687e431817121e0992afd
pmedved <philipp.medved@gmx.at>
2018-06-12 18:43:59 +0200

    added the first basic funktion for a chat

--- src/main/java/Client/ClientController.java
+++ src/main/java/Client/ClientController.java
@@ -1,4 +1,44 @@
 package Client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+
 public class ClientController {
+
+    @FXML
+    private TextArea mainTextArea;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private TextField inputTextArea;
+
+    @FXML
+    private TextArea onlineUsers;
+
+    private String username = "";
+
+
+    @FXML
+    private void send(){
+        if(username.equals("")){
+            username = JOptionPane.showInputDialog("Bitte geben Sie ihren Usernamen ein");
+            onlineUsers.setText(onlineUsers.getText() + "\n" + username);
+        }
+
+        if(mainTextArea.getText().equals("")) {
+            mainTextArea.setText(username + ": " +inputTextArea.getText());
+        }else{
+            mainTextArea.setText(mainTextArea.getText() + "\n" + username + ": " +inputTextArea.getText());
+        }
+        inputTextArea.clear();
+
+    }
+
+
 }
