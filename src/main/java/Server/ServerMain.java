78905278fe25dfdb09335ac103b8a16244bb47b4
pmedved <philipp.medved@gmx.at>
2018-06-12 17:52:41 +0200

    Gui erstellt

--- src/main/java/Server/ServerMain.java
+++ src/main/java/Server/ServerMain.java
@@ -1,4 +1,4 @@
-package Client;
+package Server;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,19 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ClientMain extends Application {
+public class ServerMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         try{
             //Layout File wird geladen
-            Parent root = FXMLLoader.load(ClassLoader.getSystemResource("ClientLayout.fxml"));
+            Parent root = FXMLLoader.load(ClassLoader.getSystemResource("ServerLayout.fxml"));
 
             //Scene wird erstellt
             Scene scene = new Scene(root);
 
             //Fenster wird erstellt
-            primaryStage.setTitle("Simple Chat Client");
+            primaryStage.setTitle("Simple Chat Server");
             primaryStage.setScene(scene);
             primaryStage.show();
         }catch (Exception e){
